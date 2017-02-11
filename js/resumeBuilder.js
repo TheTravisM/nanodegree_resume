4d77bfde86f080c677d5f51ad63a4ec6dc865544
Travis E Mikolay <travisemikolay@gmail.com>
2017-02-11 08:34:36 -0600

    Add JS and HTML Beautification

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,17 +1,17 @@
 // - [ Bio ] - //
 var bio = {
-    "name" : "Travis Mikolay",
-    "role" : "Front End Web Developer",
-    "contacts" : {
-        "mobile" : "513.405.5183",
-        "email" : "TraviseMikolay@gmail.com",
-        "github" : "github.com/TheTravisM",
+    "name": "Travis Mikolay",
+    "role": "Front End Web Developer",
+    "contacts": {
+        "mobile": "513.405.5183",
+        "email": "TraviseMikolay@gmail.com",
+        "github": "github.com/TheTravisM",
         "linkedin": "linkedin.com/in/travismikolay",
-        "twitter" : "#",
-        "location" : "Rogers, AR"
+        "twitter": "#",
+        "location": "Rogers, AR"
     },
-    "welcomeMessage" : "It's time to start living the life you've only imagined.",
-    "skills" : [
+    "welcomeMessage": "It's time to start living the life you've only imagined.",
+    "skills": [
         "HTML 5",
         "CSS 3",
         "JavaScript",
@@ -23,95 +23,93 @@ var bio = {
         "Photoshop",
         "Illustrator"
     ],
-    "biopic" : "https://scontent-dft4-1.xx.fbcdn.net/v/t1.0-9/11258133_704864350469_3452514736886572123_n.jpg?oh=55a803fe009ea6b6d08acdb4322563a6&oe=5917AA5B"
+    "biopic": "https://scontent-dft4-1.xx.fbcdn.net/v/t1.0-9/11258133_704864350469_3452514736886572123_n.jpg?oh=55a803fe009ea6b6d08acdb4322563a6&oe=5917AA5B"
     // Add Display function
 };
 
 // - [ Work ] -
 var work = {
-    "jobs": [
-        {
-            "employer" : "JB Hunt",
-            "title" : "Senior Software Programming Specialist",
-            "logo" : "images/jobs/jbhunt.png",
-            "url" : "http://www.jbhunt.com/",
-            "location" :"Rogers, Ar",
-            "dates" :"2016-2017",
-            "description" : "Job Description"
+    "jobs": [{
+            "employer": "JB Hunt",
+            "title": "Senior Software Programming Specialist",
+            "logo": "images/jobs/jbhunt.png",
+            "url": "http://www.jbhunt.com/",
+            "location": "Rogers, Ar",
+            "dates": "2016-2017",
+            "description": "Job Description"
         },
         {
-            "employer" : "Cengage Learning",
-            "title" : "Senior Front End App Developer",
-            "logo" : "images/jobs/cengage.png",
-            "url" : "http://www.cengage.com/",
-            "location" : "Mason, Ohio",
-            "dates" : "2012-2016",
-            "description" : "Web Accessibility Compliance, HTML5, CCS3, JQuery, JavaScript, SEO, Social Media"
+            "employer": "Cengage Learning",
+            "title": "Senior Front End App Developer",
+            "logo": "images/jobs/cengage.png",
+            "url": "http://www.cengage.com/",
+            "location": "Mason, Ohio",
+            "dates": "2012-2016",
+            "description": "Web Accessibility Compliance, HTML5, CCS3, JQuery, JavaScript, SEO, Social Media"
         },
         {
-            "employer" : "RDI Corp",
-            "title" : "Senior Web Developer",
-            "logo" : "images/jobs/rdi.png",
-            "url" : "http://rdicorp.com/",
-            "location" : "Blue Ash, Ohio",
-            "dates" : "2009-2010",
-            "description" : "• Spearheaded the creation of dynamic web sites for RDI and sister companies <br>• Worked with a team of programmers in a fast paced environment <br>• Created dynamic websites for the ground up for RDI and well as many of the companies that they had worked with."
+            "employer": "RDI Corp",
+            "title": "Senior Web Developer",
+            "logo": "images/jobs/rdi.png",
+            "url": "http://rdicorp.com/",
+            "location": "Blue Ash, Ohio",
+            "dates": "2009-2010",
+            "description": "• Spearheaded the creation of dynamic web sites for RDI and sister companies <br>• Worked with a team of programmers in a fast paced environment <br>• Created dynamic websites for the ground up for RDI and well as many of the companies that they had worked with."
         },
         {
-            "employer" : "Definity Partners",
-            "title" : "Web Developer",
-            "logo" : "images/jobs/definity.png",
-            "url" : "http://www.definitypartners.com/",
-            "location" : "Mason, Ohio",
-            "dates" : "2009-2010",
-            "description" : "• Spearheaded the creation of dynamic web sites for RDI and sister companies <br>• Worked with a team of programmers in a fast paced environment <br>• Created dynamic websites for the ground up for RDI and well as many of the companies that they had worked with."
+            "employer": "Definity Partners",
+            "title": "Web Developer",
+            "logo": "images/jobs/definity.png",
+            "url": "http://www.definitypartners.com/",
+            "location": "Mason, Ohio",
+            "dates": "2009-2010",
+            "description": "• Spearheaded the creation of dynamic web sites for RDI and sister companies <br>• Worked with a team of programmers in a fast paced environment <br>• Created dynamic websites for the ground up for RDI and well as many of the companies that they had worked with."
         },
         {
-            "employer" : "College of Mount St. Joseph",
-            "title" : "Web Developer",
-            "logo" : "images/jobs/msj.png",
-            "url" : "www.msj.edu/",
-            "location" : "Cincinnati, Ohio",
-            "dates" : "2009-2010",
-            "description" : "• Created and updated existing website<br>• Focused on photo editing and manipulation<br>• Specialize in Adobe Flash web design"
+            "employer": "College of Mount St. Joseph",
+            "title": "Web Developer",
+            "logo": "images/jobs/msj.png",
+            "url": "www.msj.edu/",
+            "location": "Cincinnati, Ohio",
+            "dates": "2009-2010",
+            "description": "• Created and updated existing website<br>• Focused on photo editing and manipulation<br>• Specialize in Adobe Flash web design"
         }
     ]
 };
 
 // - [ Projects ] -
 var projects = {
-    "projects" : [
-        {
-            "title" : "Health Care App",
-            "dates" : "2001",
-            "description" : "Project 1 Description",
+    "projects": [{
+            "title": "Health Care App",
+            "dates": "2001",
+            "description": "Project 1 Description",
             "github": "https://github.com/KickAssBrockSamson/AngularJS-HealthCare-UI",
             "url": "https://github.com/KickAssBrockSamson/AngularJS-HealthCare-UI",
-            "images" : [
+            "images": [
                 "images/projects/Cappadocia-Turkey-Zeki-Seferoglu.jpg",
                 "images/projects/Fairy-Tale-İlhan-Eroglu.jpg",
                 "images/projects/Lake-Tekapo-New-Zealand-Christian-Lim.jpg"
             ]
         },
         {
-            "title" : "Oracle Workbench App",
-            "dates" : "2001",
-            "description" : "Project 3 Description",
-            "github" : "https://github.com/KickAssBrockSamson/Functional-Design-Workbench",
-            "url" : "http://kickassbrocksamson.github.io/Functional-Design-Workbench/#/",
-            "images" : [
+            "title": "Oracle Workbench App",
+            "dates": "2001",
+            "description": "Project 3 Description",
+            "github": "https://github.com/KickAssBrockSamson/Functional-Design-Workbench",
+            "url": "http://kickassbrocksamson.github.io/Functional-Design-Workbench/#/",
+            "images": [
                 "images/projects/Li-Jiang-China-Trey-Ratcliff.jpg",
                 "images/projects/Lichtenstein-Castle-Germany-Martin.jpg",
                 "images/projects/Machu-Picchu-Peru-Piotr-Kulczycki.jpg"
             ]
         },
         {
-            "title" : "Web Development Guidelines",
-            "dates" : "2001",
-            "description" : "Project 2 Description",
-            "github" : "https://github.com/KickAssBrockSamson/Web-Development-Guidelines",
-            "url" : "http://kickassbrocksamson.github.io/Web-Development-Guidelines/",
-            "images" : [
+            "title": "Web Development Guidelines",
+            "dates": "2001",
+            "description": "Project 2 Description",
+            "github": "https://github.com/KickAssBrockSamson/Web-Development-Guidelines",
+            "url": "http://kickassbrocksamson.github.io/Web-Development-Guidelines/",
+            "images": [
                 "images/projects/Paptsdorf-Germany-skoeber.jpg",
                 "images/projects/Rio-de-Janeiro-Brazil-airpano.jpg",
                 "images/projects/Cappadocia-Turkey-Zeki-Seferoglu.jpg"
@@ -123,10 +121,9 @@ var projects = {
 
 // - [ Education ] -
 var education = {
-    "schools": [
-        {
-            "name" : "Mount Saint Joseph",
-            "location" : "Delhi, OH",
+    "schools": [{
+            "name": "Mount Saint Joseph",
+            "location": "Delhi, OH",
             "degree": "Bachelor of Fine Arts",
             "dates": "2004-2009",
             "url": "http://www.msj.edu/",
@@ -134,8 +131,8 @@ var education = {
             "minors": ["Illustration", "iDesign"]
         },
         {
-            "name" : "Glen Este Highschool",
-            "location" : "Cincinnati, OH",
+            "name": "Glen Este Highschool",
+            "location": "Cincinnati, OH",
             "degree": "",
             "dates": "2000-2004",
             "url": "http://www.westcler.k12.oh.us/",
@@ -143,64 +140,63 @@ var education = {
             "minors": []
         }
     ],
-    "onlineCources" : [
-        {
-            "title" : "Front-End Web Developer Nanodegree",
-            "school" : "UDacity",
+    "onlineCources": [{
+            "title": "Front-End Web Developer Nanodegree",
+            "school": "UDacity",
             "dates": 2017,
             "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
         },
         {
-            "title" : "How to Use Git and GitHub",
-            "school" : "UDacity",
+            "title": "How to Use Git and GitHub",
+            "school": "UDacity",
             "dates": 2016,
             "url": "https://www.udacity.com/course/how-to-use-git-and-github--ud775"
         },
         {
-            "title" : "Angular 2 Essential Training",
-            "school" : "Lynda.com",
+            "title": "Angular 2 Essential Training",
+            "school": "Lynda.com",
             "dates": 2016,
             "url": "https://www.lynda.com/AngularJS-tutorials/Angular-2-Essential-Training/540347-2.html?srchtrk=index%3a2%0alinktypeid%3a2%0aq%3aangular%0apage%3a1%0as%3arelevance%0asa%3atrue%0aproducttypeid%3a2"
         },
         {
-            "title" : "Learn AngularJS 2: The Basics",
-            "school" : "Lynda.com",
+            "title": "Learn AngularJS 2: The Basics",
+            "school": "Lynda.com",
             "dates": 2016,
             "url": "https://www.lynda.com/AngularJS-tutorials/Learn-AngularJS-2-Basics/428058-2.html?srchtrk=index%3a13%0alinktypeid%3a2%0aq%3aangular%0apage%3a1%0as%3arelevance%0asa%3atrue%0aproducttypeid%3a2"
         },
         {
-            "title" : "Building a Mobile App with AngularJS 1 and Ionic",
-            "school" : "Lynda.com",
+            "title": "Building a Mobile App with AngularJS 1 and Ionic",
+            "school": "Lynda.com",
             "dates": 2016,
             "url": "https://www.lynda.com/AngularJS-tutorials/Building-Mobile-App-AngularJS-1-Ionic/368920-2.html?srchtrk=index%3a16%0alinktypeid%3a2%0aq%3aangular%0apage%3a1%0as%3arelevance%0asa%3atrue%0aproducttypeid%3a2"
         },
         {
-            "title" : "Sass Essential Training",
-            "school" : "Lynda.com",
+            "title": "Sass Essential Training",
+            "school": "Lynda.com",
             "dates": 2015,
             "url": "https://www.lynda.com/Sass-tutorials/Sass-Essential-Training/375925-2.html"
         },
         {
-            "title" : "Bootstrap 3 Essential Training",
-            "school" : "Lynda.com",
+            "title": "Bootstrap 3 Essential Training",
+            "school": "Lynda.com",
             "dates": 2015,
             "url": "https://www.lynda.com/Bootstrap-tutorials/Bootstrap-3-Essential-Training/417641-2.html"
         },
         {
-            "title" : "Up and Running with Git and GitHub",
-            "school" : "Lynda.com",
+            "title": "Up and Running with Git and GitHub",
+            "school": "Lynda.com",
             "dates": 2015,
             "url": "https://www.lynda.com/Git-tutorials/Up-Running-Git-GitHub/409275-2.html"
         },
         {
-            "title" : "Building a Mobile App with AngularJS 1 and Ionic",
-            "school" : "Lynda.com",
+            "title": "Building a Mobile App with AngularJS 1 and Ionic",
+            "school": "Lynda.com",
             "dates": 2015,
             "url": "https://www.lynda.com/AngularJS-tutorials/Building-Mobile-App-AngularJS-1-Ionic/368920-2.html"
         },
         {
-            "title" : "Web Project Workflows with Gulp.js, Git, and Browserify",
-            "school" : "Lynda.com",
+            "title": "Web Project Workflows with Gulp.js, Git, and Browserify",
+            "school": "Lynda.com",
             "dates": 2014,
             "url": "https://www.lynda.com/Web-Design-tutorials/Web-Project-Workflows-Gulp-js-Git-Browserify/154416-2.html"
         }
@@ -211,17 +207,17 @@ var education = {
 // - [ Header
 
 // - - [ Bio Pic Column 1
-var formattedHTMLbioPic = HTMLbioPic.replace("%data%",bio.biopic);
+var formattedHTMLbioPic = HTMLbioPic.replace("%data%", bio.biopic);
 $("#header .column-1").append(formattedHTMLbioPic);
 
 // - - [ Contact Info Column 2
-var formattedName = HTMLheaderName.replace("%data%",bio.name);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
 $("#header .bio").append(formattedName);
-var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 $("#header .bio").append(formattedRole);
 
 // - - [ Contact Info
-var formattedHTMLmobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
+var formattedHTMLmobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
 $("#topContacts").append(formattedHTMLmobile);
 $("#topContacts li.mobile a:last").append(bio.contacts.mobile);
 var formattedHTMLemail = HTMLemail.replace("%data%", bio.contacts.email);
@@ -236,13 +232,13 @@ $("#topContacts li.github a:last").append(bio.contacts.github);
 var formattedHTMLlocation = HTMLlocation.replace("%data%", bio.contacts.location);
 $("#topContacts").append(formattedHTMLlocation);
 // -- [ Message
-var formattedHTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+var formattedHTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 $("#topContacts").after(formattedHTMLwelcomeMsg);
 
 // - [ Bio SKILLS If Statement
 $("#header .skills").append(HTMLskillsStart);
 for (var skill in bio.skills) {
-    formattedSkill = HTMLskills.replace("%data%",bio.skills[skill]);
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
     $("#skills").append(formattedSkill);
 }
 
@@ -278,8 +274,8 @@ projects.display = function() {
         $("h3.project-title a:last").append(formattedTitle);
         var formattedDates = HTMLprojectDates.replace("%data%", project.dates);
         $("h3.project-title:last").append(formattedDates);
-        if (project.images.length > 0 ) {
-            project.images.forEach(function(image){
+        if (project.images.length > 0) {
+            project.images.forEach(function(image) {
                 var formattedImage = HTMLprojectImage.replace("%data%", image);
                 $(".project-entry:last").append(formattedImage);
                 var formattedImageAlt = image.slice(16);
@@ -296,7 +292,7 @@ projects.display();
 
 // - [ Education - "School"s
 education.display = function() {
-    education.schools.forEach(function(school){
+    education.schools.forEach(function(school) {
         $("#education .container .row").append(HTMLschoolStart);
         var formattedName = HTMLschoolName.replace("%data%", school.name);
         $(".education-entry:last").append(formattedName);
@@ -306,23 +302,23 @@ education.display = function() {
         $(".education-entry:last").append(formattedDates);
         var formattedLocation = HTMLschoolLocation.replace("%data%", school.location);
         $(".education-entry:last").append(formattedLocation);
-        if(school.majors.length > 0 ){
-            school.majors.forEach(function(major){
+        if (school.majors.length > 0) {
+            school.majors.forEach(function(major) {
                 var formattedMajor = HTMLschoolMajor.replace("%data%", major);
                 $(".education-entry:last").append(formattedMajor);
             });
         }
-        if(school.minors.length > 0 ){
-            school.minors.forEach(function(minor){
+        if (school.minors.length > 0) {
+            school.minors.forEach(function(minor) {
                 var formattedMinor = HTMLschoolMinor.replace("%data%", minor);
                 $(".education-entry:last").append(formattedMinor);
             });
         }
     });
-    if(education.onlineCources.length > 0 ){
+    if (education.onlineCources.length > 0) {
         $(".education-entry:last").append(HTMLonlineClasses);
     }
-    education.onlineCources.forEach(function(course){
+    education.onlineCources.forEach(function(course) {
         var formattedURL = HTMLonlineURL.replace("%data%", course.url);
         $(".education-entry:last").append(formattedURL);
         var formattedTitle = HTMLonlineTitle.replace("%data%", course.title);
@@ -341,7 +337,7 @@ $("#mapDiv").append(googleMap);
 // -- [ Footer
 
 // - - [ Contact Info
-var formattedHTMLmobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
+var formattedHTMLmobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
 $("#lets-connect").append(formattedHTMLmobile);
 $("#lets-connect li.mobile a:last").append(bio.contacts.mobile);
 var formattedHTMLemail = HTMLemail.replace("%data%", bio.contacts.email);
